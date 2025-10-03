96125ca3bd089520ec9ead13a82c58a09bfff8b8
mandrindraa <mandrindraantonnio@gmail.com>
2025-10-03 19:04:38 +0300

    main works!

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,3 @@
-#![allow(dead_code)]
-#![allow(unused_imports)]
-use std::collections::{BTreeMap, HashMap, HashSet, VecDeque};
-use std::io::Error;
-use std::time::Duration;
-
-fn main() {
-    let mut hash_map: HashMap<String, String> = HashMap::<String, String>::new();
-    (&mut hash_map).insert("foo".to_string(), "bar".to_string());
-    let mut btree_map: BTreeMap<String, String> = BTreeMap::new();
-    (&mut btree_map).insert("foo".to_string(), "bar".to_string());
-    let mut queue: VecDeque<i32> = VecDeque::<i32>::new();
-    for i in 1..=900 {
-        if i % 68 == 0 {
-            (&mut queue).push_back(i);
-        }
-    }
+fn main(){
+    println!("{} works!", file!());
 }
